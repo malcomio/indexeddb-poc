62e82b77fee744cf73798dd92780c2e7bb74e7ff
Malcolm Young <428782+malcomio@users.noreply.github.com>
2021-11-04 11:56:57 +0000

    set localforage config

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import { createStore } from 'vuex'
 import VuexPersistence from 'vuex-persist';
 import localForage from 'localforage';
 
+localForage.config({
+  storeName: 'myCustomStoreName'
+});
+
 const vuexLocal = new VuexPersistence({
   storage: localForage,
   asyncStorage: true
